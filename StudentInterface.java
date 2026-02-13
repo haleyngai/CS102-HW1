9dcebabebaba61c22f7c8b9b00b095e63b794160
haleyngai <136915333+haleyngai@users.noreply.github.com>
2026-02-13 11:57:21 -0500

    Student Interface

--- StudentInterface.java
+++ StudentInterface.java
@@ -1,3 +1,12 @@
+import java.util.ArrayList; 
+// req 06: interface for student with method signature
 public interface StudentInterface {
-    
+  
+	void viewAllCourses();
+	void viewFullCourses();
+	void registerOnCourse(String student);
+	void sortCoursesByEnrollment(String firstName, String lastName);
+
+    ArrayList<Course> getCourses();
+	
 }
